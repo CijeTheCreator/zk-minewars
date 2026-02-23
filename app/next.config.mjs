a6e3161586f6f4981fd1e6d06e9263009de07899
Chijioke Osadebe <101515459+CijeTheCreator@users.noreply.github.com>
2026-02-23 19:43:45 +0100

    Fixed next.config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  experimental: {
+    serverComponentsExternalPackages: ["@aztec/bb.js", "@noir-lang/noir_js"],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
